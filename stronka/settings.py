5baf346109321d61d89d5d9ede49cda096183a66
Marcin <ty@adres.pl>
2017-06-24 17:08:57 +0200

    Poprawka settings

--- stronka/settings.py
+++ stronka/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'zi#1*6&cup^n@s-3)86u=r+p4+ygljjmia-j@&bl119^)yv%0&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hipolopos.pythonanywhere.com']
 
 
 # Application definition
